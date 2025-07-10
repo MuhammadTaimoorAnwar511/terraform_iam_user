6a54e44cf2e637845604b49e6fdbb5ad8036ddaf
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-07-10 16:54:09 +0500

    first commit

---
+++ iam_output.tf
@@ -0,0 +1,23 @@
+# iam_output.tf
+output "iam_username" {
+  description = "The IAM user name"
+  value       = aws_iam_user.user.name
+}
+
+output "iam_password" {
+  description = "The IAM console password (user may be forced to reset depending on variable)"
+  value       = aws_iam_user_login_profile.login.password
+  sensitive   = true
+}
+
+output "access_key_id" {
+  description = "Programmatic access key ID"
+  value       = aws_iam_access_key.user_key.id
+  sensitive   = true
+}
+
+output "secret_access_key" {
+  description = "Programmatic secret access key"
+  value       = aws_iam_access_key.user_key.secret
+  sensitive   = true
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region               = "us-east-1"
+iam_user_name            = "taimoor-s3-iam-user"
+iam_force_password_reset = false                                         # false = no forced reset; true = require reset
+policy_arn               = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # s3 full access
